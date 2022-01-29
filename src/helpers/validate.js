9b8b0c0e4791ca1e07d7b26dd83d97accd20f023
sinelnikov-web <sinelnikov-web@mail.ru>
2022-01-29 23:40:05 +0600

    add spaces validation

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -1,9 +1,9 @@
-
-
 export function validate(fields, validationSchema) {
     let errors = Object.keys(fields).reduce((acc, key) => ({...acc, [key]: ''}), {})
+    let localFields = {...fields}
+    Object.keys(localFields).forEach(key => localFields[key] = localFields[key].trim())
     try {
-        validationSchema.validateSync(fields, {abortEarly: false})
+        validationSchema.validateSync(localFields, {abortEarly: false})
     } catch (err) {
         err.inner.forEach(error => {
             if (error.path) {
